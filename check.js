391bc363073ecfd07644915818b96233ae25311f
h4b0 <h4b0@h4b0.pl>
2015-04-30 12:03:30 +0200

    First version

---
+++ check.js
@@ -0,0 +1,14 @@
+if(window.getSelection().type == "Range"){
+  var movie = window.getSelection().toString();
+  var url = 'https://www.yify-torrent.org/search/'+movie+'/'
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("GET", url, false);
+  xmlHttp.send(null);
+  var status = xmlHttp.status;
+  if (status == 404)
+	alert("Film not found!");
+  else
+	window.open(url);
+} else {
+	alert("Nothing selected.")
+}
